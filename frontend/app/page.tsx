6040f8332c1c9b444a0bddb38613fc17fe8239ca
Yellon3k <w.dawid99@vp.pl>
2025-10-30 16:35:39 +0100

    update global styles and main page layout

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,11 @@
+import Chat from '@/components/Chat';
+import Sidebar from '@/components/Sidebar';
 
 export default function Home() {
   return (
-    <></>
+    <main>
+      <Chat />
+      <Sidebar />
+    </main>
   );
 }
